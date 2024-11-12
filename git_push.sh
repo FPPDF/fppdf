f83ec31de35aebe792ef370f7534cac4c05e56bb
LucianHL <l.harland-lang@ucl.ac.uk>
2024-11-12 15:50:50 +0000

    Test upload

--- git_push.sh
+++ git_push.sh
@@ -4,5 +4,5 @@
 git remote add origin git@github.com:LucianHL/fixpar_nnpdf.git
 git add *
 git commit -m "Test upload"
-git checkout -b main
-git push origin main
+git checkout -b lhl_test
+git push origin lhl_test
